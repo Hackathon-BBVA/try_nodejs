7c1b31888cf7e171a1ef53b61199fb0220e88aed
Jordan Rodriguez <jrodriguezm216@gmail.com>
2020-10-18 04:52:45 -0500

    50% Landing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import '../assets/css/main.css';
+import '../public/assets/css/main.css';
 
 class App extends Component {
   render() {
